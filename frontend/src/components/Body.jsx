4d810ebf21c79704a3c1de15f47066086c4e8282
unknown <yusuf7samaila@gmail.com>
2021-11-15 21:45:45 +0100

    body page edited

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,16 +1,48 @@
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import Image from 'react-bootstrap/Image'
+import '../index.css'
+import farmer from '../images/farmer.jpg'
 
 function Body() {
     return (
         <Container>
-            <h3 className="mt-5 text-center">What We Do</h3>
+            <h3 className="mt-5 text-center Fjalla">Who We Are</h3>
             <Row className="mt-5" style={{color:'#bc6c25'}}>
-                <Col className="text-center" variant="#bc6c25">
-                    <h4>We're Farmers' best choice</h4>
+                <Col className="">
+                    <h4 className="Fjalla text-center">We're Farmers' best choice</h4>
+                    <p>
+                        Agrily provides farmers with a reliable and a trusted platform to 
+                        enlist their farm produce, and get a buyer in a very short time, 
+                        with a very good price. <br/> Because Agrily cares loves to help
+                         farmers sell their farm produces while it is still fresh.
+                         Farmers have made Agrily their most trusted crop trading platform. <br/>
+                         If you are a farmer, kindly sign up and enjoy the benefits of getting your
+                          farm produce to the buyers within the fastest time possible. 
+
+                    </p>
                 </Col>
-                <Col>2 of 3</Col>
+                <Col>
+                    <Image src={farmer} fluid />
+                </Col>
+                
+            </Row>
+
+
+            <Row className="mt-5" style={{color:'#bc6c25'}}>
+                <Col className="" sm={12} md={6}>
+                    <Image src={farmer} fluid />
+                </Col>
+                <Col sm={12} md={6}>
+                <h4 className="Fjalla text-center">Buyers prefer Agrily</h4>
+                    <p>
+                        Buyers prefer to buy farm produce through Agrily. Getting a high quality produce has never been easier,
+                        thanks to Agrily. Because we have the buyers in mind, we are the best choice for buyers looking to purchase 
+                        fresh, and quality farm produce.
+                    </p>
+                    
+                </Col>
                 
             </Row>
         </Container>
