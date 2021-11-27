9f96bda44498da9a7d63adca9395259454986f9f
Yusuf Samaila <yusuf7samaila@gmail.com>
2021-11-27 21:52:01 +0100

    fixes

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,9 @@ const path = require('path');
 require('dotenv/config')
 const userRoute = require('./routes/user')
 const cors = require('cors')
+app.use(cors())
 
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
 
-app.use(cors(corsOptions));
 const userRouter = require('./routes/user');
 
 
